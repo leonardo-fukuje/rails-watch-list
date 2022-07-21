c5e1173d659065f8e30f55ab558ff5b679eae152
leonardo fukuje <leonardo.fukuje@gmail.com>
2022-07-21 13:46:24 -0300

    added feature that allows user to delete specific movie

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
   has_one_attached :photo
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
